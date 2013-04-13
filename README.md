08543fff309745fbfdea51e0aa12df8715131a31
Matheus Braun Magrin <braunmagrin@gmail.com>
2013-04-13 18:43:18 -0300

    Initial code and README file

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ Octave-class-creator
 ====================
 
 Creates a Octave class and its files to achieve a nice initial behaviour.
+The script creates the directory to contain the class files, in which it then
+creates the class constructor and functions to allow subscripted references and
+subscripted assignments.
+
+Usage:
+
+    create_class.py <CLASS_NAME> <ATTR_NAME> [<ATTR_NANE> ...]
+
+Example:  
+To create a class called 'car' which have the following attributes:  
+
+*    make;  
+*    color;  
+*    year.  
+
+One may execute: 
+ 
+    ./create_class.py car make color year
+
+Result:
+The script will create a directory named @car and will create inside
+of it three files:
+
+*    `car.m` - the constructor;
+*    `subsref.m` - to allow subscripted references like `c.make`;
+*    `subsasgn.m` - to allow subscripted assignments like `c.make = 'make-name'`
---
+++ create_class.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+if len(sys.argv) < 2:
+	raise Exception('Not enough parameters')
+
+class_name = sys.argv[1]
+attributes_list = sys.argv[2:]
+
+# create directory
+dir_name = '@' + class_name
+if not os.path.exists(dir_name):
+	os.makedirs(dir_name)
+
+# constructor
+begin = "\
+function object = {0}()\n"
+
+attr_clause = "\
+object.{} = [];\n"
+
+end = "\
+object = class(object, '{}');\n\
+end"
+
+with open(dir_name + '/' + class_name + '.m', 'w') as out:
+	out.write(begin.format(class_name))
+	for attr in attributes_list:
+		out.write(attr_clause.format(attr))
+	out.write(end.format(class_name))
+	
+# subsref
+begin = "\
+function ret = subsref(obj, sub)\n\
+if isempty(sub)\n\
+	error('@{0}/subsref: missing index');\n\
+end\n\n\
+switch sub.type\n\
+case '.'\n\
+	attribute = sub.subs;\n\
+	switch attribute\n"
+
+case_clause = "\
+	case '{0}'\n\
+		ret = obj.{0};\n"
+
+end = "\
+	otherwise\n\
+		error(\"@{0}/subsref: invalid property '%s'\", attribute);\n\
+	end\n\
+otherwise\n\
+	error(\"@{0}/subsref: invalid property '%s'\", attribute);\n\
+end\n\n\
+end"
+
+with open(dir_name + '/subsref.m','w') as out:
+	out.write(begin.format(class_name))
+	for attr in attributes_list:
+		out.write(case_clause.format(attr))
+	out.write(end.format(class_name))
+	
+# subsasgn
+begin = "\
+function ret = subsasgn(obj, sub, rhs)\n\
+if isempty(sub)\n\
+	error('@{0}/subsasgn: missing index');\n\
+end\n\n\
+ret = obj;\n\n\
+switch sub.type\n\
+case '.'\n\
+	attribute = sub.subs;\n\
+	switch attribute\n"
+
+case_clause = "\
+	case '{0}'\n\
+		ret.{0} = rhs;\n"
+
+end = "\
+	otherwise\n\
+		error(\"@{0}/subsasgn: invalid property '%s'\", attribute);\n\
+	end\n\
+otherwise\n\
+	error(\"@{0}/subsasgn: invalid property '%s'\", attribute);\n\
+end\n\n\
+end"
+
+with open(dir_name + '/subsasgn.m','w') as out:
+	out.write(begin.format(class_name))
+	for attr in attributes_list:
+		out.write(case_clause.format(attr))
+	out.write(end.format(class_name))
